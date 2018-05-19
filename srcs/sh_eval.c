828e91f94a585c399672c07721c813839cf0777f
Kelian DUMARAIS <kdumarai@student.42.fr>
2018-05-19 02:30:49 +0200

    now more crappy fixed size pid_t tab

--- srcs/sh_eval.c
+++ srcs/sh_eval.c
@@ -6,7 +6,7 @@
 /*   By: kdumarai <kdumarai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/12 22:22:21 by kdumarai          #+#    #+#             */
-/*   Updated: 2018/05/19 01:47:49 by kdumarai         ###   ########.fr       */
+/*   Updated: 2018/05/19 02:29:42 by kdumarai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,28 +15,34 @@
 #include <signal.h>
 #include "sh.h"
 
-static void			clean_pipes(pid_t *pids)
+static void			clean_pipes(t_tab *pids)
 {
-	while (*pids != -1)
+	unsigned int	idx;
+	pid_t			*curr;
+
+	idx = pids->oc_size;
+	while (idx--)
 	{
-		if (*pids > 0 && waitpid(*pids, NULL, WNOHANG) == 0)
+		curr = &((pid_t*)pids->data)[idx];
+		if (*curr > 0 && waitpid(*curr, NULL, WNOHANG) == 0)
 		{
-			kill(*pids, SIGKILL);
-			waitpid(*pids, NULL, 0);
-			*pids = 0;
+			kill(*curr, SIGKILL);
+			waitpid(*curr, NULL, 0);
+			*curr = 0;
 		}
-		pids++;
 	}
 }
 
-static t_cmdnode	*eval_pipe(t_cmdnode *a, t_cmdnode *b, pid_t *pids)
+static t_cmdnode	*eval_pipe(t_cmdnode *a, t_cmdnode *b, t_tab *pids)
 {
 	int		pfd[2];
+	pid_t	pid;
 
 	pipe(pfd);
 	a->stdout_fd = pfd[1];
 	b->stdin_fd = pfd[0];
-	exec_cmd(a, YES, pids, NULL);
+	exec_cmd(a, YES, &pid, NULL);
+	ft_ttabcat(pids, &pid, sizeof(pid_t));
 	close(pfd[1]);
 	return (b);
 }
@@ -47,7 +53,7 @@ static t_cmdnode	*eval_semi(t_cmdnode *a, t_cmdnode *b)
 	return (b);
 }
 
-static t_cmdnode	*eval_ast(t_btree *node, pid_t *pids)
+static t_cmdnode	*eval_ast(t_btree *node, t_tab *pids)
 {
 	t_astnode	*ndat;
 	t_cmdnode	*leftnode;
@@ -73,17 +79,17 @@ int					eval_line(char *line)
 	int		ret;
 	t_list	*tokens;
 	t_btree	*ast;
-	pid_t	pids[11];
+	t_tab	pids;
 
 	if (!(tokens = lex_line(line)))
 		return (EXIT_SUCCESS);
 	parser_check_heredocs(tokens);
 	if (!(ast = parse_tokens(tokens)))
 		return (ft_returnmsg("eval_line: ast error", STDERR_FILENO, 258));
-	ft_bzero(pids, sizeof(pids) - sizeof(pid_t));
-	pids[sizeof(pids) / sizeof(pid_t) - 1] = -1;
-	ret = exec_cmd(eval_ast(ast, pids), NO, NULL, NULL);
-	clean_pipes(pids);
+	pids = ft_ttabnew(sizeof(pid_t));
+	ret = exec_cmd(eval_ast(ast, &pids), NO, NULL, NULL);
+	clean_pipes(&pids);
+	ft_ttabdel(&pids);
 	ft_btdel(&ast, &ast_btdel);
 	ft_lstdel(&tokens, &tokens_lstdel);
 	set_env_var_n(NULL, "?", ret);
