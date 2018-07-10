bb29706cb26744bd0ae35896a31036331eb877b9
Kelian DUMARAIS <kdumarai@student.42.fr>
2018-07-10 23:19:39 +0200

    start syntax check

--- srcs/parser/parser_syntax.c
+++ srcs/parser/parser_syntax.c
@@ -6,7 +6,7 @@
 /*   By: kdumarai <kdumarai@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/05/21 17:10:58 by kdumarai          #+#    #+#             */
-/*   Updated: 2018/05/21 20:19:18 by kdumarai         ###   ########.fr       */
+/*   Updated: 2018/07/10 22:54:53 by kdumarai         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,10 @@ t_uint8		parser_check_syntax(t_list *tokens, t_uint8 ragain)
 	{
 		curr = (t_token*)tokens->content;
 		if (curr->type == PIPE && (!prev || prev->priority != 0))
+			return (FALSE);
+		if (curr->type >= GREAT && curr->type <= DLESS && (!tokens->next
+			|| (((t_token*)tokens->next->content)->type != WORD
+				&& ((t_token*)tokens->next->content)->type != AMPERSAND)))
 			return (FALSE);
 		prev = curr;
 		tokens = tokens->next;
